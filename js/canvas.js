7bf7b2d3e74593f122d1809cbb4f16340119855a
Daniil <daniil322@gmail.com>
2019-12-11 17:15:00 +0200

    third commit

--- js/canvas.js
+++ js/canvas.js
@@ -1,24 +1,35 @@
 let canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d"),
   gCanvasDraws = [],
-  drawId = 1
-  gCurrClickedID=0,
-  gDragMode=false;
+  drawId = 1;
+(gCurrClickedID = 0), (gDragMode = false);
 
 function canvasImage(memeURL) {
   base_image = new Image();
   base_image.src = memeURL;
   base_image.onload = function() {
     ctx.drawImage(base_image, 0, 0);
-    canvasWrite()
+    canvasWrite();
   };
 }
-function canvasWrite(txt, textSize = 50, color = "#000000", x = 180, y = 80) {
-  ctx.font = `${textSize}px Impact`;
+function canvasWrite(
+  txt='hello',
+  textSize = 50,
+  color = "#000000",
+  x = 180,
+  y = 80,
+  stroke = "#000000",
+  id=drawId++,
+) {
   ctx.fillStyle = color;
+  ctx.strokeStyle = stroke;
+  ctx.font = `${textSize}px Impact`;
   ctx.fillText(txt, x, y);
-  gCanvasDraws.push({
-    id: (memeId = drawId++),
+  ctx.strokeText(txt, x, y);
+  ctx.fill();
+  ctx.stroke();
+  gCanvasDraws.unshift({
+    stroke: stroke,
     wordWidth: ctx.measureText(txt).width,
     wordHeight: textSize * 1.5,
     type: canvasWrite,
@@ -26,13 +37,17 @@ function canvasWrite(txt, textSize = 50, color = "#000000", x = 180, y = 80) {
     textSize,
     color,
     x,
-    y
+    y,
+    id
   });
 }
 
 function changeDraw(elInput) {
-    let fontSize=   gCanvasDraws[gCurrClickedID].textSize
+  let fontSize = gCanvasDraws[gCurrClickedID].textSize;
   switch (elInput.name) {
+    case "borderColor":
+      gCanvasDraws[gCurrClickedID].stroke = elInput.value;
+      break;
     case "text":
       gCanvasDraws[gCurrClickedID].txt = elInput.value;
       break;
@@ -40,12 +55,12 @@ function changeDraw(elInput) {
       gCanvasDraws[gCurrClickedID].color = elInput.value;
       break;
     case "fontUp":
-        fontSize++
-      gCanvasDraws[gCurrClickedID].textSize = fontSize
+      fontSize++;
+      gCanvasDraws[gCurrClickedID].textSize = fontSize;
       break;
     case "fontDown":
-        fontSize--
-      gCanvasDraws[gCurrClickedID].textSize = fontSize
+      fontSize--;
+      gCanvasDraws[gCurrClickedID].textSize = fontSize;
       break;
   }
   drawCanvas();
@@ -56,13 +71,11 @@ function findLine(id) {
 }
 
 function drawCanvas() {
-    let canvas = document.querySelector("canvas")
-  ctx = canvas.getContext("2d")
   ctx.drawImage(base_image, 0, 0);
-  gCanvasDraws.forEach((draw,i)=> {
+  gCanvasDraws.forEach((draw, i) => {
     let temp = draw.type;
-    temp(draw.txt, draw.textSize, draw.color, draw.x, draw.y);
-    gCanvasDraws.splice(i,1)
+    temp(draw.txt, draw.textSize, draw.color, draw.x, draw.y,draw.stroke,draw.id);
+    gCanvasDraws.splice(i, 1);
   });
 }
 
@@ -77,19 +90,27 @@ function checkClick(ev) {
       x > draw.x &&
       y > draw.y - draw.wordHeight / 2
     ) {
-        if(gDragMode){
-            debugger
-            draw.x=x
-            draw.y=y
-            return drawCanvas()
-        }
+    //   if (gDragMode) {
+    //     draw.x = x;
+    //     draw.y = y;
+    //     gDragMode = false;
+    //     return drawCanvas();
+    //   }
       document.querySelector(".text").value = draw.txt;
       document.querySelector(".color").value = draw.color;
-      gCurrClickedID=draw.id
+      gCurrClickedID = draw.id-1;
     }
   });
 }
-
- function gDragMode(){
-    gDragMode=true
+function deleteDraw() {
+  gCanvasDraws.splice(gCurrClickedID, 1);
+  drawCanvas();
 }
+function addLine() {
+  canvasWrite('hi',50,'',200,200);
+  gCurrClickedID=gCanvasDraws.length-1
+}
+
+// function dragMode() {
+//   gDragMode = true;
+// }
