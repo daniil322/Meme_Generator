cbdba71a8ae9628d304274e80b1b82802b92b252
Daniil <daniil322@gmail.com>
2019-12-11 18:24:04 +0200

    fourth commit

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,10 @@
 let canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d"),
   gCanvasDraws = [],
-  drawId = 1;
-(gCurrClickedID = 0), (gDragMode = false);
+  drawId = 1,
+  gCurrClickedIDX = 0,
+  gDragWords = false,
+  gDragMode = false;
 
 function canvasImage(memeURL) {
   base_image = new Image();
@@ -13,13 +15,13 @@ function canvasImage(memeURL) {
   };
 }
 function canvasWrite(
-  txt='hello',
+  txt = "hello",
   textSize = 50,
   color = "#000000",
   x = 180,
   y = 80,
   stroke = "#000000",
-  id=drawId++,
+  id = drawId++
 ) {
   ctx.fillStyle = color;
   ctx.strokeStyle = stroke;
@@ -28,7 +30,7 @@ function canvasWrite(
   ctx.strokeText(txt, x, y);
   ctx.fill();
   ctx.stroke();
-  gCanvasDraws.unshift({
+  gCanvasDraws.push({
     stroke: stroke,
     wordWidth: ctx.measureText(txt).width,
     wordHeight: textSize * 1.5,
@@ -43,24 +45,24 @@ function canvasWrite(
 }
 
 function changeDraw(elInput) {
-  let fontSize = gCanvasDraws[gCurrClickedID].textSize;
+  let fontSize = gCanvasDraws[gCurrClickedIDX].textSize;
   switch (elInput.name) {
     case "borderColor":
-      gCanvasDraws[gCurrClickedID].stroke = elInput.value;
+      gCanvasDraws[gCurrClickedIDX].stroke = elInput.value;
       break;
     case "text":
-      gCanvasDraws[gCurrClickedID].txt = elInput.value;
+      gCanvasDraws[gCurrClickedIDX].txt = elInput.value;
       break;
     case "color":
-      gCanvasDraws[gCurrClickedID].color = elInput.value;
+      gCanvasDraws[gCurrClickedIDX].color = elInput.value;
       break;
     case "fontUp":
       fontSize++;
-      gCanvasDraws[gCurrClickedID].textSize = fontSize;
+      gCanvasDraws[gCurrClickedIDX].textSize = fontSize;
       break;
     case "fontDown":
       fontSize--;
-      gCanvasDraws[gCurrClickedID].textSize = fontSize;
+      gCanvasDraws[gCurrClickedIDX].textSize = fontSize;
       break;
   }
   drawCanvas();
@@ -72,15 +74,32 @@ function findLine(id) {
 
 function drawCanvas() {
   ctx.drawImage(base_image, 0, 0);
-  gCanvasDraws.forEach((draw, i) => {
+  gCanvasDraws.forEach(draw => {
     let temp = draw.type;
-    temp(draw.txt, draw.textSize, draw.color, draw.x, draw.y,draw.stroke,draw.id);
-    gCanvasDraws.splice(i, 1);
+    temp(
+      draw.txt,
+      draw.textSize,
+      draw.color,
+      draw.x,
+      draw.y,
+      draw.stroke,
+      draw.id
+    );
+    gCanvasDraws.pop();
   });
+}
+function moveTo(ev) {
+  if (gDragWords === false) return;
+  let x = ev.offsetX;
+  let y = ev.offsetY;
+  if (gDragMode) {
+    gCanvasDraws[gDragWords].x = x;
+    gCanvasDraws[gDragWords].y = y;
+    return drawCanvas();
+  }
 }
 
 function checkClick(ev) {
-  event.stopPropagation();
   let x = ev.offsetX;
   let y = ev.offsetY;
   gCanvasDraws.forEach(draw => {
@@ -90,27 +109,30 @@ function checkClick(ev) {
       x > draw.x &&
       y > draw.y - draw.wordHeight / 2
     ) {
-    //   if (gDragMode) {
-    //     draw.x = x;
-    //     draw.y = y;
-    //     gDragMode = false;
-    //     return drawCanvas();
-    //   }
       document.querySelector(".text").value = draw.txt;
       document.querySelector(".color").value = draw.color;
-      gCurrClickedID = draw.id-1;
+      gCurrClickedIDX = findLine(draw.id);
+      gDragWords = findLine(draw.id);
+      gDragMode = true;
     }
   });
 }
 function deleteDraw() {
-  gCanvasDraws.splice(gCurrClickedID, 1);
+  gCanvasDraws.splice(gCurrClickedIDX, 1);
   drawCanvas();
 }
 function addLine() {
-  canvasWrite('hi',50,'',200,200);
-  gCurrClickedID=gCanvasDraws.length-1
+  canvasWrite("hi", 50, "", 200, 200);
+  gCurrClickedIDX = gCanvasDraws.length - 1;
 }
 
-// function dragMode() {
-//   gDragMode = true;
-// }
+function exitDragMode() {
+  gDragMode = false;
+  gDragWords = false;
+}
+function downloadCanvas(elDownload) {
+  image = canvas.toDataURL("image/png", 1.0);
+  elDownload.download = "my-image.png";
+  elDownload.href = image;
+  elDownload.click();
+}
